700673eb5f121e83b7b5f287140c7f78b4e04e9f
Lucas Eiji <104649020+eijilucas@users.noreply.github.com>
2023-05-11 16:40:04 -0300

    Update ConnectionFactory.java

--- ConnectionFactory.java
+++ ConnectionFactory.java
@@ -11,13 +11,15 @@ import java.sql.DriverManager;
  * @author lucas
  */
 public class ConnectionFactory {
+    //Define os atributos user, url e a senha
     protected static final String User = "postgres";
     protected static final String Password = "1234";
     protected static final String Url = "jdbc:postgresql://localhost:5432/Produto";
     
+    //Cria a classe de conexão com o banco de dados através do driver JDBC, fazendo um link com o banco de dados e a aplicação.
     public static Connection getConexao() throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-        Connection con  = DriverManager.getConnection(Url, User, Password);
-        return con;
+        Class.forName("org.postgresql.Driver"); //Chama o driver
+        Connection con  = DriverManager.getConnection(Url, User, Password); //Define os parâmetros de conexão
+        return con; //Cria a conexão
     }
 }
